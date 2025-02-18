b15ed6c533ab5a86012f9f70920c4ee0e28192a3
KaioCarv <kaiocarvalho558@gmail.com>
2025-02-17 23:57:12 -0300

    [API] Módulo de Transactions fazendo o CRUD das Transections e utilizando query filters para os filtros de datas da aplicação

---
+++ api/src/modules/transactions/transactions.controller.ts
@@ -0,0 +1,64 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  ParseUUIDPipe,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { ActiveUserId } from 'src/shared/decorators/ActiveUserId';
+import { CreateTransactionDto } from './dto/create-transaction.dto';
+import { UpdateTransactionDto } from './dto/update-transaction.dto';
+import { TransactionsService } from './services/transactions.service';
+
+@Controller('transactions')
+export class TransactionsController {
+  constructor(private readonly transactionsService: TransactionsService) {}
+
+  @Post()
+  create(
+    @ActiveUserId() userid: string,
+    @Body() createTransactionDto: CreateTransactionDto,
+  ) {
+    return this.transactionsService.create(userid, createTransactionDto);
+  }
+
+  @Get()
+  findAll(
+    @ActiveUserId() userId: string,
+    @Query('month', ParseIntPipe) month: number,
+    @Query('year', ParseIntPipe) year: number,
+    @Query('bankAccountId', ParseUUIDPipe) bankAccountId?: number,
+  ) {
+    console.log({ bankAccountId });
+    return this.transactionsService.findAllByUserId(userId, { month, year });
+  }
+
+  @Put(':transactionId')
+  update(
+    @ActiveUserId() userId: string,
+    @Param('transactionId', ParseUUIDPipe) transactionId: string,
+    @Body() updateTransactionDto: UpdateTransactionDto,
+  ) {
+    return this.transactionsService.update(
+      userId,
+      transactionId,
+      updateTransactionDto,
+    );
+  }
+
+  @Delete(':transactionId')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  remove(
+    @ActiveUserId() userId: string,
+    @Param('transactionId', ParseUUIDPipe) transactionId: string,
+  ) {
+    return this.transactionsService.remove(userId, transactionId);
+  }
+}
--- api/src/shared/database/database.module.ts
+++ api/src/shared/database/database.module.ts
@@ -2,6 +2,7 @@ import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 import { BankAccountsRepository } from './repositories/bank-account.repositories';
 import { CategoriesRepository } from './repositories/categories.repositories copy';
+import { TransactionsRepository } from './repositories/transactions.repositories ';
 import { UsersRepository } from './repositories/users.repositories';
 
 @Global()
@@ -11,7 +12,13 @@ import { UsersRepository } from './repositories/users.repositories';
     UsersRepository,
     CategoriesRepository,
     BankAccountsRepository,
+    TransactionsRepository,
   ],
-  exports: [UsersRepository, CategoriesRepository, BankAccountsRepository],
+  exports: [
+    UsersRepository,
+    CategoriesRepository,
+    BankAccountsRepository,
+    TransactionsRepository,
+  ],
 })
 export class DatabaseModule {}
