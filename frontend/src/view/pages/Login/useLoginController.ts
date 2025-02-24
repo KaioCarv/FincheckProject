bd658b6822a7277009f7e5830fa04c2e18204795
KaioCarv <kaiocarvalho558@gmail.com>
2025-02-24 19:36:47 -0300

    Fazendo a a validação do formulario utilizando HookForm e zod

---
+++ frontend/src/view/pages/Login/useLoginController.ts
@@ -0,0 +1,26 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const schema = z.object({
+  email: z.string().nonempty('E-mail é obrigatório').email('Informe um email válido'),
+  password: z.string().nonempty('Senha é obrigatório').min(8, 'Senha deve conter pelo menos 8 dígitos')
+})
+
+type FormData = z.infer<typeof schema>
+
+export function useLoginController(){
+const {
+  register,
+  handleSubmit: hookFormHandleSubmit,
+  formState: {errors}
+} = useForm<FormData>({
+  resolver: zodResolver(schema)
+});
+
+const handleSubmit = hookFormHandleSubmit((data) => {
+console.log('chama a Api:', data)
+})
+
+return {handleSubmit, register, errors };
+}
