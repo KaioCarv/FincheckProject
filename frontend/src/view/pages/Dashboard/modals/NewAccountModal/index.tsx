339b600c5f3e21a6f6f5488f0c9a8b612524d5fb
KaioCarv <kaiocarvalho558@gmail.com>
2025-03-19 14:59:08 -0300

    Feat: Iniciando a integração com o back-end pelo modal de criação de contas

--- frontend/src/view/pages/Dashboard/modals/NewAccountModal/index.tsx
+++ frontend/src/view/pages/Dashboard/modals/NewAccountModal/index.tsx
@@ -1,3 +1,4 @@
+import { Controller } from "react-hook-form";
 import { Button } from "../../../../components/Button";
 import { ColorsDropDownInput } from "../../../../components/ColorsDropDownInput";
 import { Input } from "../../../../components/Input";
@@ -9,7 +10,11 @@ import { useNewAccountModalController } from "./useNewAccountModalController";
 export function NewAccountModal(){
   const {
     closeNewAccountModal,
-    isNewAccountModalOpen
+    isNewAccountModalOpen,
+    errors,
+    handleSubmit,
+    register,
+    control,
   } = useNewAccountModalController()
   return (
     <Modal
@@ -17,24 +22,38 @@ export function NewAccountModal(){
       open={isNewAccountModalOpen}
       onClose={closeNewAccountModal}
     >
-     <form>
+     <form onSubmit={handleSubmit}>
          <div>
-          <span className="text-gray-600 tracking-[-0.5px] text-xs">Saldo</span>
+          <span className="text-gray-600 tracking-[-0.5px] text-xs">Saldo inicial</span>
             <div className="flex items-center gap-2">
               <span className="text-gray-600 tracking-[-0.5px] text-lg">R$</span>
-              <InputCurrency/>
+              <Controller
+                defaultValue="0"
+                control={control}
+                name="initialBalance"
+                render={({field: {onChange, value} }) => (
+                  <InputCurrency
+                  error={errors.initialBalance?.message}
+                  onChange={onChange}
+                  value={value}
+                  />
+                )}
+              />
             </div>
         </div>
 
         <div className="mt-10 flex flex-col gap-4 ">
           <Input
             type="text"
-            name="name"
             placeholder="Nome da Conta"
+            error={errors.name?.message}
+            {...register('name')}
+
           />
 
           <Select
           placeholder="Tipo"
+          error={errors.type?.message}
           options={[
             {
               value: 'CHECKING',
@@ -50,7 +69,9 @@ export function NewAccountModal(){
             },
           ]}
           />
-          <ColorsDropDownInput/>
+          <ColorsDropDownInput
+            error={errors.color?.message}
+          />
         </div>
           <Button type="submit" className="w-full mt-6">
             Criar
--- frontend/src/view/pages/Dashboard/modals/NewAccountModal/useNewAccountModalController.ts
+++ frontend/src/view/pages/Dashboard/modals/NewAccountModal/useNewAccountModalController.ts
@@ -1,4 +1,16 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 import { useDashboard } from "../../components/DashboardContext/useDashboard";
+
+const schema = z.object({
+  initialBalance: z.string().nonempty('Saldo inicial é obrigatorio'),
+  name: z.string().nonempty('Nome da Conta é obrigatorio'),
+  type: z.enum(['CHECKING', 'INVESTMENT', 'CASH']),
+  color: z.string().nonempty('Cor é obrigatorio'),
+})
+
+type FormData = z.infer<typeof schema>
 
 export function useNewAccountModalController(){
   const {
@@ -6,9 +18,25 @@ export function useNewAccountModalController(){
     closeNewAccountModal,
   } = useDashboard()
 
+  const {
+    register,
+    handleSubmit: hookFormSubmit,
+    formState: {errors},
+    control,
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+  });
+
+  const handleSubmit = hookFormSubmit(async (data) => {
+    console.log({data})
+  })
 
   return {
     isNewAccountModalOpen,
     closeNewAccountModal,
+    register,
+    errors,
+    handleSubmit,
+    control,
   }
 }
