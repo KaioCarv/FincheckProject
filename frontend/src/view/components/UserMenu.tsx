93dfe3dce6728272c08c73a8eb2fcd5f04de5acb
KaioCarv <kaiocarvalho558@gmail.com>
2025-04-07 12:22:08 -0300

    Adicionando capa

--- frontend/src/view/components/UserMenu.tsx
+++ frontend/src/view/components/UserMenu.tsx
@@ -4,12 +4,13 @@ import { DropdownMenu } from "./DropdownMenu";
 
 export function UserMenu(){
   const {signout, user} = useAuth();
+  console.log({user})
   return (
     <DropdownMenu.Root>
     <DropdownMenu.Trigger>
       <div className="bg-teal-50 rounded-full w-12 h-12 flex items-center justify-center border border-teal-100">
         <span className="text-sm tracking-[-0.5px] text-teal-900 font-medium">
-          {user?.name.slice(0, 2).toLocaleUpperCase()}
+         KC
         </span>
       </div>
     </DropdownMenu.Trigger>
